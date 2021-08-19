2150fa3a321b39a107d32c5a25a5ade9c577c33d
Rwabukanga <sethfils2016@gmail.com>
2021-08-19 16:42:01 -0700

    First Commit.

---
+++ RsProject_RsProject/src/main/java/com/RsProject/RsProject/RsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.RsProject.RsProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsProjectApplication.class, args);
+	}
+
+}
---
+++ RsProject_RsProject/src/test/java/com/RsProject/RsProject/RsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RsProject.RsProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
